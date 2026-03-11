4022a1a58397b663a09f9bc9d56e796ba6588e5d
Pritam Mehta <131163935+pritammehta01@users.noreply.github.com>
2026-03-11 17:43:53 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,9 @@ app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_origins=[
+        "http://35.238.104.251:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
